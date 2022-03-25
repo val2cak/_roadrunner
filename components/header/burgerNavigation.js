128190022892dd7fb5e044ef5ac111eedff7b7fa
val2cak <valentina.tucak99@gmail.com>
2022-03-25 09:17:42 +0100

    refactoring

--- components/header/burgerNavigation.js
+++ components/header/burgerNavigation.js
@@ -4,13 +4,13 @@ const BurgerNavigation = ({ isOpen, navigationItems, setIsOpen }) => {
     return (
         <main
             className={`${
-                !isOpen ? 'translate-x-full' : 'translate-x-0'
+                !isOpen ? 'translate-x-full text-white' : 'translate-x-0'
             } top-0 transform translate fixed left-0 m-0 p-0 bg-hci-header text-white lg:hidden w-screen transition duration-500 ease-in-out flex-col justify-center items-center h-screen z-40`}
             onClick={() => setIsOpen(!isOpen)}
         >
             <NavBar
                 navigationItems={navigationItems}
-                classes='flex flex-col h-full w-full justify-center items-center text-white'
+                className='flex flex-col h-full w-full justify-center items-center text-white'
             />
         </main>
     );
