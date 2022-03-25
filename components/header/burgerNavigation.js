b1909a0b426534fd5d291a54edeb965666ecc1ef
val2cak <valentina.tucak99@gmail.com>
2022-03-25 09:42:56 +0100

    refactoring

--- components/header/burgerNavigation.js
+++ components/header/burgerNavigation.js
@@ -8,10 +8,19 @@ const BurgerNavigation = ({ isOpen, navigationItems, setIsOpen }) => {
             } top-0 transform translate fixed left-0 m-0 p-0 bg-hci-header text-white lg:hidden w-screen transition duration-500 ease-in-out flex-col justify-center items-center h-screen z-40`}
             onClick={() => setIsOpen(!isOpen)}
         >
-            <NavBar
-                navigationItems={navigationItems}
-                classes='flex flex-col h-full w-full justify-center items-center text-white'
-            />
+            <div className='pl-16 no-underline text-white font-regular font-bold text-lg pt-32'>
+                <div className='pl-10 text-3xl text-white'>
+                    <Link href='/'>
+                        <a className='no-underline z-10 pb-6 items-center text-white text-3xl md:text-2xl lg:text-3xl font-regular font-bold hover:text-hci-general-middle'>
+                            roadrunner
+                        </a>
+                    </Link>
+                </div>
+                <NavBar
+                    navigationItems={navigationItems}
+                    classes='flex flex-col h-full w-full justify-center items-center'
+                />
+            </div>
         </main>
     );
 };
